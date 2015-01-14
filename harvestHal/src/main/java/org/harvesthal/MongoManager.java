a88f37ac9d4db0f2bf30c47a1334c7988e4eb6b4
Azhar <achraf.azhar@inria.fr>
2015-01-14 10:41:11 +0100

    Use gridfs document uploadDate instead of classifying using collection.
    -Unless we increase the 'hard limit', we got hash collisions when using a large bunch of collections !
    see more here : https://groups.google.com/forum/#!topic/mongodb-user/YrPqM85N4Mk

--- harvestHal/src/main/java/org/harvesthal/MongoManager.java
+++ harvestHal/src/main/java/org/harvesthal/MongoManager.java
@@ -11,18 +11,28 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.UnknownHostException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 import java.util.Properties;
 
 public class MongoManager {
-    
+
+    public static final String OAI_NAMESPACE = "oairesponses";
+    public static final String OAI_TEI_NAMESPACE = "oaiteis";
+    public static final String BINARY_NAMESPACE = "binarynamespaces";
+    public static final String GROBID_NAMESPACE = "teigrobidnamespaces";
+
     public static String filePath = "";
     public String mongodbServer = "";
-    public int mongodbPort ;
+    public int mongodbPort;
     public String mongodbDb = "";
-    public DB db= null;
-    
+    public DB db = null;
+
     public MongoManager() {
-        try{
+        try {
             Properties prop = new Properties();
             prop.load(new FileInputStream("harvestHal.properties"));
             mongodbServer = prop.getProperty("harvestHal.mongodbServer");
@@ -34,47 +44,37 @@ public class MongoManager {
             e.printStackTrace();
         }
     }
-    
-    public void storeToGridfs(InputStream file, String fileName, String namespace) {
+
+    public void storeToGridfs(InputStream file, String fileName, String namespace, String dateString) throws ParseException {
 
         try {
             GridFS gfs = new GridFS(db, namespace);
             GridFSInputFile gfsFile = gfs.createFile(file, true);
+            gfsFile.put("uploadDate", parseStringDate(dateString));
             gfsFile.setFilename(fileName);
             gfsFile.save();
 
         } catch (MongoException e) {
         }
     }
-    
-    synchronized public void  storeToGridfs(String filePath, String fileName, String namespace) throws IOException {
+
+    public void storeToGridfs(String filePath, String fileName, String namespace, String dateString) throws IOException, ParseException {
 
         try {
             GridFS gfs = new GridFS(db, namespace);
             File f = new File(filePath);
             GridFSInputFile gfsFile = gfs.createFile(f);
+            gfsFile.put("uploadDate", parseStringDate(dateString));
             gfsFile.setFilename(fileName);
             gfsFile.save();
 
         } catch (MongoException e) {
         }
     }
-    
-    public String loadFromGridfs(String fileName, String namespace){
-        try {
-            Mongo mongo = new Mongo(mongodbServer, mongodbPort);
-            DB db = mongo.getDB(mongodbDb);
-            GridFS gfsPhoto = new GridFS(db, namespace);
-            GridFSDBFile documentForOutput = gfsPhoto.findOne(fileName);
-            
-            documentForOutput.writeTo("/Users/Achraf/tmp/"+fileName);
-            
-            System.out.println("Done");
-        } catch (UnknownHostException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return filePath;
+
+    private Date parseStringDate(String dateString) throws ParseException {
+        DateFormat format = new SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH);
+        Date date = format.parse(dateString);
+        return date;
     }
 }
