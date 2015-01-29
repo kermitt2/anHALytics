3920ee3ce378d1fdf0f5e042fb5c7c9c2fd7ad83
Azhar <achraf.azhar@inria.fr>
2015-01-29 15:09:49 +0100

    Add grobid process.

--- harvestHal/src/main/java/org/harvesthal/MongoManager.java
+++ harvestHal/src/main/java/org/harvesthal/MongoManager.java
@@ -2,8 +2,10 @@ package org.harvesthal;
 
 import com.mongodb.MongoClient;
 import com.mongodb.DB;
+import com.mongodb.DBObject;
 import com.mongodb.MongoException;
 import com.mongodb.gridfs.GridFS;
+import com.mongodb.gridfs.GridFSDBFile;
 import com.mongodb.gridfs.GridFSInputFile;
 import java.io.File;
 import java.io.FileInputStream;
@@ -13,6 +15,7 @@ import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
 import java.util.Properties;
 
@@ -21,7 +24,7 @@ public class MongoManager {
     public static final String OAI_NAMESPACE = "oairesponses";
     public static final String OAI_TEI_NAMESPACE = "oaiteis";
     public static final String BINARY_NAMESPACE = "binarynamespaces";
-    public static final String GROBID_NAMESPACE = "teigrobidnamespaces";
+    public static final String GROBID_NAMESPACE = "halheader_grobidbody";
 
     public static String filePath = "";
     public DB db = null;
@@ -47,6 +50,7 @@ public class MongoManager {
 
         try {
             GridFS gfs = new GridFS(db, namespace);
+            gfs.remove(fileName);
             GridFSInputFile gfsFile = gfs.createFile(file, true);
             gfsFile.put("uploadDate", parseStringDate(dateString));
             gfsFile.setFilename(fileName);
@@ -61,11 +65,28 @@ public class MongoManager {
         try {
             GridFS gfs = new GridFS(db, namespace);
             File f = new File(filePath);
+            gfs.remove(fileName);
             GridFSInputFile gfsFile = gfs.createFile(f);
             gfsFile.put("uploadDate", parseStringDate(dateString));
             gfsFile.setFilename(fileName);
             gfsFile.save();
 
+        } catch (MongoException e) {
+        }
+    }
+    
+    public void loadBinaries(String tmpPath) throws IOException {
+        String[] filename;
+        try {
+            GridFS gfs = new GridFS(db, BINARY_NAMESPACE);
+            List<GridFSDBFile> files = gfs.find((DBObject)null);
+            for (GridFSDBFile file : files) {
+                filename = file.getFilename().split("\\.");
+                 File temp = File.createTempFile(filename[0]+"_", "."+filename[1], new File(tmpPath));
+                 temp.deleteOnExit();
+                 file.writeTo(temp);
+                 System.out.println(temp.getAbsolutePath());
+            }
         } catch (MongoException e) {
         }
     }
