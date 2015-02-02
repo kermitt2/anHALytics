f095c9aa3bfe4b8febe3876699de0e1edad57cb1
Lemmet <achraf.azhar@inria.fr>
2015-02-02 11:12:14 +0100

    Update corresponding grobidService tests.

--- harvestHal/src/test/java/org/harvesthal/TestGrobidService.java
+++ harvestHal/src/test/java/org/harvesthal/TestGrobidService.java
@@ -26,7 +26,6 @@ public class TestGrobidService extends BaseTest {
         } catch (Exception e) {
             e.printStackTrace();
         }
-        ExecutorService executor = Executors.newFixedThreadPool(2);
             String grobid_host = prop.getProperty("harvestHal.grobid_host");
 	    String grobid_port = prop.getProperty("harvestHal.grobid_port");
 		File pdfFile = new File(this.getResourceDir("src/test/resources/").getAbsoluteFile() + 
@@ -34,9 +33,8 @@ public class TestGrobidService extends BaseTest {
 		if (!pdfFile.exists()) {
 			throw new Exception("Cannot start test, because test resource folder is not correctly set.");
 		}
-		Future<String> submit = executor.submit(
-			new GrobidService(pdfFile.getPath(), grobid_host, grobid_port, 2, -1, true));                  
-		String fulltext = submit.get();
+		GrobidService grobidService = new GrobidService(pdfFile.getPath(), grobid_host, grobid_port, 2, -1, true);                  
+		String fulltext = grobidService.runFullTextGrobid();
 		// some test here...
 		//System.out.println(fulltext);
 		FileUtils.writeStringToFile(new File(this.getResourceDir("src/test/resources/").getAbsoluteFile() + 
@@ -47,13 +45,12 @@ public class TestGrobidService extends BaseTest {
 		if (!pdfFile.exists()) {
 			throw new Exception("Cannot start test, because test resource folder is not correctly set.");
 		}
-		submit = executor.submit(new GrobidService(pdfFile.getPath(), grobid_host, grobid_port, 2, -1, true)); 
-		fulltext = submit.get();
+		grobidService = new GrobidService(pdfFile.getPath(), grobid_host, grobid_port, 2, -1, true);                  
+		fulltext = grobidService.runFullTextGrobid();
 		// some test here...
 		//System.out.println(fulltext);
 		FileUtils.writeStringToFile(new File(this.getResourceDir("src/test/resources/").getAbsoluteFile() + 
 			"/hal-01110668v1.fulltext.tei.xml"), fulltext, "UTF-8");
-		
 		/*pdfFile = new File(this.getResourceDir("src/test/resources/").getAbsoluteFile() + 
 			"/main.pdf");
 		if (!pdfFile.exists()) {
